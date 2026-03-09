f96a588b405c4e98e155068fd87a2318c62e8c26
leogracas34-gif <leogracas34@gmail.com>
2026-03-09 18:42:54 -0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Arquivo de configuração de build de nível superior.
+// Aqui definimos os plugins que serão aplicados aos módulos do projeto.
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
