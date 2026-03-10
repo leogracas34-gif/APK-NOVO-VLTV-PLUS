d2b596a6db6076e4a2136d2e8df4dde37a107d4e
leogracas34-gif <leogracas34@gmail.com>
2026-03-10 16:41:44 -0300

    Update SeriesEntity.kt

--- app/src/main/java/com/vltvplus/iptv/SeriesEntity.kt
+++ app/src/main/java/com/vltvplus/iptv/SeriesEntity.kt
@@ -3,15 +3,15 @@ package com.vltvplus.iptv
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// Atualizado para "series_streams" para bater com o StreamDao e IptvRepository
+// O nome da tabela DEVE ser "series_streams" para bater com o seu StreamDao
 @Entity(tableName = "series_streams")
 data class SeriesEntity(
     @PrimaryKey 
-    val series_id: Int, // Alterado para snake_case para bater com o padrão da sua Database V6
+    val series_id: Int,
     val name: String,
     val cover: String?,
     val rating: String?,
-    val category_id: String, // Alterado para snake_case
-    val last_modified: Long, // Alterado para Long conforme o erro de Cursor do Room
+    val category_id: String,
+    val last_modified: Long,
     val logo_url: String? = null
 )
