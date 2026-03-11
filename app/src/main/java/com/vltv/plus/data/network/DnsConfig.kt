193942b7a03080420f4e8afc68bd275b0ddcae60
leogracas34-gif <leogracas34@gmail.com>
2026-03-10 23:08:22 -0300

    Create DnsConfig.kt

---
+++ app/src/main/java/com/vltv/plus/data/network/DnsConfig.kt
@@ -0,0 +1,18 @@
+package com.vltv.plus.data.network
+
+object DnsConfig {
+    val DNS_LIST = listOf(
+        "http://fibercdn.sbs",           ← DNS 1
+        "http://tvblack.shop",           ← DNS 2
+        "http://redeinternadestiny.top", ← DNS 3
+        "http://blackstartv.shop",       ← DNS 4
+        "http://blackdns.shop",          ← DNS 5
+        "http://blackdeluxe.shop",       ← DNS 6
+        "http://ranos.sbs",              ← DNS 7
+        "http://cmdtv.casa",             ← DNS 8
+        "http://cmdtv.pro",              ← DNS 9
+        "http://cmdtv.sbs",              ← DNS 10
+        "http://cmdtv.top",              ← DNS 11
+        "http://cmdbr.life"              ← DNS 12
+    )
+}
