14b315a5381058a0fc045665dd40ae8ec713fc09
leogracas34-gif <leogracas34@gmail.com>
2026-03-10 22:47:33 -0300

    Update MainActivity.kt

--- app/src/main/java/com/vltv/plus/MainActivity.kt
+++ app/src/main/java/com/vltv/plus/MainActivity.kt
@@ -2,10 +2,22 @@ package com.vltv.plus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vltv.plus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        // Carregar LoginFragment por padrão
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.fragmentContainer.id, LoginFragment())
+                .commit()
+        }
     }
 }
