4d469ccc4c76fd3954f3bdb447ac35707a983c85
leogracas34-gif <leogracas34@gmail.com>
2026-03-10 22:25:15 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-// Arquivo de configuração de build de nível superior.
-// Aqui definimos os plugins que serão aplicados aos módulos do projeto.
-
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 }
